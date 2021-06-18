a339fb84b2a13a9ecd2061445c52914c69d4d910
eternalsaki <44955382+siotomeakito@users.noreply.github.com>
2021-06-19 04:39:19 +0800

    Add files via upload

---
+++ mqtt.c
@@ -0,0 +1,116 @@
+﻿#include "mqtt.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+//input your message char array, client Id, user name, password
+uint16_t mqtt_connect(char * buff, const char *id, const char *user, const char *pass)
+{
+ 	//set header to CONNECT(0001 0000)
+ 	buff[0] = 0x10;
+ 	
+ 	uint16_t length = MQTT_MAX_HEADER_SIZE;
+ 	
+    //set mqtt version
+ 	#if MQTT_VERSION == MQTT_VERSION_3_1
+ 	uint8_t d[9] = {0x00,0x06,'M','Q','I','s','d','p', MQTT_VERSION};
+ 	#define MQTT_HEADER_VERSION_LENGTH 9
+ 	#elif MQTT_VERSION == MQTT_VERSION_3_1_1
+ 	uint8_t d[7] = {0x00,0x04,'M','Q','T','T',MQTT_VERSION};
+ 	#define MQTT_HEADER_VERSION_LENGTH 7
+ 	#endif
+
+ 	for (int j = 0;j<MQTT_HEADER_VERSION_LENGTH;j++) {
+ 		buff[length++] = d[j];
+ 	}
+ 	
+ 	uint8_t v = 0x00;	//set "FLAG" (1 byte)
+ 	v = v|0x02;			//set "clean session", automatically clean the setting of client after client disconnecting
+ 	v = v|0x80;			//use the "user name"
+ 	v = v|(0x80>>1);	//use the "password"
+ 	buff[length++] = v;
+ 	
+ 	buff[length++] = ((MQTT_KEEPALIVE) >> 8);       //keep alive, char: 8bit ; uint16_t: 16bit. cut MQTT_KEEPALIVE to 2 parts
+ 	buff[length++] = ((MQTT_KEEPALIVE) & 0xFF);
+
+ 	length = mqtt_writeBuffer(buff, length, id, 1);  
+ 	length = mqtt_writeBuffer(buff, length, user, 1);
+ 	length = mqtt_writeBuffer(buff, length, pass, 1);
+ 	buff[MQTT_MAX_HEADER_SIZE-1] = (length-MQTT_MAX_HEADER_SIZE);	//RemainLength
+ 	
+ 	if(length > MQTT_MAX_PACKET_SIZE || length == 0)
+ 	{
+ 	return 0;
+ 	}
+ 	else{
+ 	return length;
+ 	}
+}
+
+//QOS 0
+//input your message char array, topic, payload
+uint16_t mqtt_publish(char * buff, const char* topic, const char* payload)
+{
+    buff[0] = 0x30;  //QOS 0
+        
+    // Leave room in the buffer for header and variable length field
+    uint16_t length = MQTT_MAX_HEADER_SIZE;
+        
+    length = mqtt_writeBuffer(buff, length, topic, 1);
+    length = mqtt_writeBuffer(buff, length, payload, 0);
+    buff[MQTT_MAX_HEADER_SIZE-1] = (length-MQTT_MAX_HEADER_SIZE);
+
+    if(length > MQTT_MAX_PACKET_SIZE || length == 0)
+    {
+        return 0;
+    }
+    else{
+        return length;
+    }
+}
+
+uint16_t mqtt_subscribe(char * buff, const char* topic, uint16_t MsgId)
+{
+    buff[0] = 0x82;
+    
+    uint16_t length = MQTT_MAX_HEADER_SIZE;
+
+    buff[length++] = (MsgId >> 8);
+    buff[length++] = (MsgId & 0xFF);
+    length = mqtt_writeBuffer(buff, length, topic, 1);
+    buff[length++] = 0x00;  //QOS 0
+    buff[MQTT_MAX_HEADER_SIZE-1] = (length-MQTT_MAX_HEADER_SIZE);
+    
+    if(length > MQTT_MAX_PACKET_SIZE || length == 1)
+    {
+        return 0;
+    }
+    else{
+        return length;
+    }
+}
+
+uint16_t mqtt_writeBuffer(char * buff, uint16_t pos, const char* str, const uint8_t counter)
+{
+	uint8_t tmpPos = pos;
+	uint8_t strlength = strnlen(str, SETTING_STR_MAX_LEN);
+    if (strlength >= SETTING_STR_MAX_LEN || pos == 0) {
+        return 0;
+    }
+    
+	char tmpArray[SETTING_STR_MAX_LEN+1] = {0};
+	if(counter){
+        *(buff+tmpPos) = ((strlength) >> 8);
+        tmpPos++;
+        *(buff+tmpPos) = ((strlength) & 0xFF);
+        tmpPos++;
+    }
+    
+	sprintf(tmpArray, "%s", str);
+	for (int i = 0;i<strlength;i++) {
+		*(buff+tmpPos) = tmpArray[i];
+        tmpPos++;
+	}
+	return tmpPos;
+}
---
+++ mqtt.h
@@ -0,0 +1,52 @@
+﻿#ifndef MQTT_H_
+#define MQTT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define MQTT_VERSION_3_1      3
+#define MQTT_VERSION_3_1_1    4
+
+// MQTT_VERSION : Pick the version
+//#define MQTT_VERSION MQTT_VERSION_3_1
+#ifndef MQTT_VERSION
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+#endif
+
+
+// MQTT_MAX_PACKET_SIZE : Maximum packet size
+#ifndef MQTT_MAX_PACKET_SIZE
+#define MQTT_MAX_PACKET_SIZE 128
+#endif
+
+// MQTT_KEEPALIVE : keepAlive interval in Seconds
+#ifndef MQTT_KEEPALIVE
+#define MQTT_KEEPALIVE 60
+#endif
+
+// Maximum size of fixed header and variable length size header
+#define MQTT_MAX_HEADER_SIZE 2	//Beta, so we fix it  to 2.
+                                //The value of the "Remaining Length" in header is according to the packet size (count the bytes of the content), and the maximum is 4 bytes.
+                                //(Hex, 256 in 1 byte, so 4 bytes means the length of the content could be up to 256^4 bytes)
+                                //Therefore the MAX HEADER SIZE should be setted 5. So we need a function to extend the header's length automatically.
+                                //BTW the MQTT_MAX_PACKET_SIZE is 128 bytes, so I think setting to 2 is enough.
+
+#define MQTTCONNECT     1 << 4  // Client request to connect to Server
+#define MQTTPUBLISH     3 << 4  // Publish message
+#define MQTTDISCONNECT  14 << 4 // Client is Disconnecting
+
+#define SETTING_STR_MAX_LEN 50
+
+uint16_t mqtt_connect(char * buff, const char *id, const char *user, const char *pass);
+uint16_t mqtt_publish(char * buff, const char* topic, const char* payload);
+uint16_t mqtt_subscribe(char * buff, const char* topic, uint16_t MsgId);
+uint16_t mqtt_writeBuffer(char * buff, uint16_t pos, const char* str, const uint8_t counter);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MQTT_H_ */
